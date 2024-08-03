0cc664599bec3d58098c2472f3919924273a6026
vngu0581 <vngu0581@uni.sydney.edu.au>
2024-08-04 02:48:00 +1000

    Added buttons to switch songs

---
+++ script.js
@@ -0,0 +1,21 @@
+// script.js
+function read(id, url) {
+    fetch(url)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.text();
+    })
+    .then(data => {
+        document.getElementById(id).innerHTML = data;
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
+}
+
+function display(name){
+    read('original', 'songs/' + name + '/Original.txt'); 
+    read('translation', 'songs/' + name + '/Translation.txt')
+}
