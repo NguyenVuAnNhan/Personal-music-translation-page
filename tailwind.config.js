3136bfc908f002f317e2197cc74684b2e4356e87
vngu0581 <annhan0328@gmail.com>
2024-08-08 22:53:59 +1000

    update: included tailwinds css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+  './*.html', // Adjust this path according to your project structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
